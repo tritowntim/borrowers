1f30dd5ab24b4621011c01f56d8a3d1e36d5535f
Tim Dussinger <tritowntim@gmail.com>
2014-12-20 00:16:26 -0500

    Working Ember <--> Rails, as triggered by console

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('friends', function() { });
 });
 
 export default Router;
+
+// Use Ember Inspector to retrieve data from Rails app 
+// $E.store.find('friend').then(function(friends) { friends.forEach(function(friend) { console.log('Hi from ' + friend.get('firstName')); }) });
