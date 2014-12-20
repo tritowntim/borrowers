7e7c4e861f0308cf2eb08f1fb4cdf8cf89c4051f
Tim Dussinger <tritowntim@gmail.com>
2014-12-20 13:39:43 -0500

    Working cancel buttons

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   actions: {
 
     save: function() {
@@ -8,6 +9,11 @@ export default Ember.Controller.extend({
       this.get('model').save().then(function(friend) {
         that.transitionToRoute('friends.show', friend);
       });
+    },
+
+    cancel: function() {
+      this.get('model').rollback();
+      this.transitionToRoute('friends.show', this.get('model'));
     }
 
   }
--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,19 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   actions: {
+
     save: function() {
-      console.log('+- save action in friends new controller');
       var that = this;
       this.get('model').save().then(function() {
         that.transitionToRoute('friends');
       });
-      return true;
     },
 
     cancel: function() {
-      console.log('+- console action in friends new controller');
-      return true;
+      this.get('model').deleteRecord();
+      this.transitionToRoute('friends');
     }
+
   }
 });
