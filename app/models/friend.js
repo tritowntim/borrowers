cdb426017875b21c0db8721455390f5316ceb1f3
Tim Dussinger <tritowntim@gmail.com>
2014-12-20 23:02:48 -0500

    Compute full name on friend model

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
-  totalArticles: DS.attr('number')
+  totalArticles: DS.attr('number'),
+  fullName: function() {
+    var first = this.get('firstName') || '';
+    var last = this.get('lastName') || '';
+    var combined = first + ' ' + last;
+    return combined.trim() ? combined : '(new)';
+  }.property('firstName', 'lastName')
 });
