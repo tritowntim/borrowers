dce38cb3fbf62c23db710ec6b966785f98b9d25f
Tim Dussinger <tritowntim@gmail.com>
2014-12-30 21:16:02 -0500

    Fontello font library

--- Brocfile.js
+++ Brocfile.js
@@ -19,4 +19,10 @@ var app = new EmberApp();
 
 app.import('bower_components/picnic/releases/v2.min.css')
 
+app.import('vendor/fontello/fontello.css')
+app.import('vendor/fontello/font/fontello.ttf',  { destDir: 'font' })
+app.import('vendor/fontello/font/fontello.eot',  { destDir: 'font' })
+app.import('vendor/fontello/font/fontello.svg',  { destDir: 'font' })
+app.import('vendor/fontello/font/fontello.woff', { destDir: 'font' })
+
 module.exports = app.toTree();
